f1739e48efea5390653668b1ac13172f8d6e956a
Tanvir Anowar <tanvir.net@gmail.com>
2021-03-31 11:09:25 +0100

    Basket controller added.

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Basket.API.Entities;
+using Basket.API.Repositories;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _basketRepository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _basketRepository.GetBasket(userName);
+            return Ok(basket);
+        }
+
+
+    }
+}
---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,36 @@
+﻿using Basket.API.Entities;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redis;
+
+        public BasketRepository(IDistributedCache redis)
+        {
+            _redis = redis;
+        }
+        
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redis.GetStringAsync(userName);
+
+            return string.IsNullOrEmpty(basket) ? null : JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+           await _redis.SetStringAsync(basket.UserName,JsonConvert.SerializeObject(basket));
+           return await GetBasket(basket.UserName);
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _redis.RemoveAsync(userName) ;
+        }
+    }
+}
