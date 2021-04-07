6ef0a5b93acd970bc0022dcf4353937af95d628f
Tanvir Anowar <tanvir.net@gmail.com>
2021-04-07 10:32:57 +0100

    Entity and valueobject operator added.

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ordering.Domain.Common
+{
+    public class EntityBase
+    {
+        public int Id { get; protected set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime LastModifiedAt { get; set; }
+
+    }
+}
