bd0ae6b81e91dcd33d8788b01dda438b04922185
Tanvir Anowar <tanvir.net@gmail.com>
2021-04-08 09:07:43 +0100

    Order deletion and exception handler added.

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using FluentValidation.Results;
+
+namespace Ordering.Application.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public Dictionary<string, string[]> Error { get;}
+
+        public ValidationException(Dictionary<string, string[]> error)
+        {
+            Error = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures)
+        {
+            Error = failures
+                .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+        }
+
+
+    }
+}
